28ebbd0a927ac97e3089d7c66a7ee669028db9da
Ali-A <ali.albabily8@gmail.com>
2019-11-09 18:16:08 +0100

    Created a new component + few changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Home page</h1>
+      <br></br>
+      <h2>Login with Google</h2>
     </div>
   );
 }
---
+++ src/components/search-movies.component.jsx
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+
+class searchMovies extends Component {
+    state = { 
+        movies: []
+    }
+
+    render() { 
+        return ( 
+            <div>
+                <h2>Search for movies and Tv-shows: </h2>
+            </div> 
+        );
+    }
+}
+
+export default searchMovies;
