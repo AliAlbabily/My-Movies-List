51dad3ca3951bd61d4a09eac43e0c3425674b1d9
Ali-A <ali.albabily8@gmail.com>
2019-11-09 17:56:09 +0100

    First commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is runinning on port: ${port}`);
+});
+
+// ***   MongoDB Connection   ***/
+// const connection = mongoose.connection;
+
+// mongoose.connect('mongodb://localhost:27017/*****', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+// connection.once('open', () => {
+//     console.log("MongoDB database connection established successfully");
+// })
+
+// ***   Make the app accept Endpoints through routes   ***/
+// const usersRouter = require('./routes/users');
+// const exercisesRouter = require('./routes/exercises');
+
+// app.use('/users', usersRouter);
+// app.use('/exercises', exercisesRouter);
