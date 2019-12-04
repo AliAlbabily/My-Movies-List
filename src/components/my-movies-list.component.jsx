4f405c78927787cbf4bb03e973b2f702a54ef1d3
Ali-A <ali.albabily8@gmail.com>
2019-12-04 20:49:41 +0100

    Now it is possible to filter movies

--- src/components/my-movies-list.component.jsx
+++ src/components/my-movies-list.component.jsx
@@ -10,7 +10,12 @@ class MyMoviesList extends Component {
         super(props);
 
         this.state = {
-            allMovies: []
+            allMovies: [],
+            currentlyWatching: [],
+            planningToWatch: [],
+            watchedMovies: [],
+            noFilteringIsMade: true,
+            runFilter: () => {}
         }
     }
 
@@ -20,9 +25,11 @@ class MyMoviesList extends Component {
             .then(
                 response => {
                     this.setState({
-                        allMovies: response.data 
+                        allMovies: response.data,
+                        currentlyWatching: response.data.filter(movieObj => movieObj.status === 'Watching'),
+                        planningToWatch: response.data.filter(movieObj => movieObj.status === 'Watch later'),
+                        watchedMovies: response.data.filter(movieObj => movieObj.status === 'Watched')
                     })
-                    console.log(this.state.allMovies)
                 }
             )
             .catch((error) => {
@@ -30,11 +37,35 @@ class MyMoviesList extends Component {
             })
     }
 
-    printMovies() {
+    getAllMovies() {
         let itemNumberCounter = 0; 
         return this.state.allMovies.map(movieObj => {
             itemNumberCounter++;
             return <SavedMovie movie={movieObj} number={itemNumberCounter} deleteMovie={this.deleteMovie} key={movieObj._id} />
+        })
+    }
+
+    getCurrentlyWatching() {
+        let itemNumberCounter = 0; 
+        return this.state.currentlyWatching.map(movieObj => {
+            itemNumberCounter++;
+            return <SavedMovie movie={movieObj} number={itemNumberCounter} deleteMovie={this.deleteMovie} key={movieObj._id} />
+        })
+    }
+
+    getPlanningToWatch() {
+        let itemNumberCounter = 0; 
+        return this.state.planningToWatch.map(movieObj => {
+            itemNumberCounter++;
+            return <SavedMovie movie={movieObj} number={itemNumberCounter} deleteMovie={this.deleteMovie} key={movieObj._id} />
+        })
+    }
+
+    getWathedMovies() {
+        let itemNumberCounter = 0; 
+        return this.state.watchedMovies.map(movieObj => {
+            itemNumberCounter++;
+            return <SavedMovie movie={movieObj} number={itemNumberCounter} deleteMovie={this.deleteMovie} key={movieObj._id} />   
         })
     }
 
@@ -48,22 +79,60 @@ class MyMoviesList extends Component {
 
     render() { 
         return ( 
-            <div>
-                <table className="content-table">
-                    <thead>
-                        <tr>
-                            <th># <i className="fas fa-sort-numeric-down"></i></th>
-                            <th><i className="fas fa-images"></i> Image</th>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Status</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.printMovies()}
-                    </tbody>
-                </table>
+            <div className="moviesList-container">
+                <div className="moviesList-c1">
+                    <div className="moviesList-c1-filter-btn" onClick={() => {
+                            this.setState({
+                                noFilteringIsMade: true,
+                                runFilter: this.getAllMovies()
+                            })
+                        }}>
+                        All Movies
+                    </div>
+                    <div className="moviesList-c1-filter-btn" onClick={() => {
+                            this.setState({
+                                noFilteringIsMade: false,
+                                runFilter: this.getCurrentlyWatching()
+                            })
+                        }}>
+                        Watching
+                    </div>
+                    <div className="moviesList-c1-filter-btn" onClick={() => {
+                            this.setState({
+                                noFilteringIsMade: false,
+                                runFilter: this.getPlanningToWatch()
+                            })
+                        }}>
+                        Plan to Watch
+                    </div>
+                    <div className="moviesList-c1-filter-btn" onClick={() => {
+                            this.setState({
+                                noFilteringIsMade: false,
+                                runFilter: this.getWathedMovies()
+                            })
+                        }}>
+                        Watched
+                    </div>
+                </div>
+                <div className="moviesList-c2">
+                    <table className="content-table">
+                        <thead>
+                            <tr>
+                                <th># <i className="fas fa-sort-numeric-down"></i></th>
+                                <th><i className="fas fa-images"></i> Image</th>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {   
+                                this.state.noFilteringIsMade ? this.getAllMovies() : this.state.runFilter 
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
