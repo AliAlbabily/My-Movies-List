e9ce13ecbf78a4c11a7b45eb147dc84aa4ed6b23
Ali-A <ali.albabily8@gmail.com>
2020-10-26 19:01:29 +0100

    Unnecessary usage of State in Movie.component

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -6,17 +6,12 @@ import '../App.css';
 class Movie extends Component {
     constructor(props) {
         super(props);
-
-        this.state = { 
-            movieId: props.movieObj.imdbID,
-            movieSource: props.movieObj.Poster
-        }
     }
 
-    render() { 
+    render() {
         return ( 
-            <Link to={"/movies/"+this.state.movieId} className="container-movie-link">
-                <img src={this.state.movieSource} alt="" />
+            <Link to={"/movies/"+this.props.movieObj.imdbID} className="container-movie-link">
+                <img src={this.props.movieObj.Poster} alt="" />
             </Link>
         );
     }
