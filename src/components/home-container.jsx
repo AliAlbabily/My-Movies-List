cf309e379f76ade67d7fec8f0c317aab5daa179c
Ali-A <ali.albabily8@gmail.com>
2020-11-24 23:26:30 +0100

    Minor changes

--- src/components/home-container.jsx
+++ src/components/home-container.jsx
@@ -6,7 +6,7 @@ class Home extends Component {
         return ( 
             <div className="home-div-wrapper row">
                 <div className="home-div-container">
-                    <h1>Save all of your favorite shows in one place. Sign in to create your own list!</h1>
+                    <h1>Save all your favorite shows in one place. Sign in to create your own list!</h1>
                     <p>This website was created and designed by Ali Albabily</p>
                 </div>
             </div>
