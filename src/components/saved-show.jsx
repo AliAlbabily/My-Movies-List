62549fc189530bdd406b92b0194887fea735a095
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-09-08 00:31:20 +0200

    Saved-show is now a functional component

--- src/components/saved-show.jsx
+++ src/components/saved-show.jsx
@@ -1,36 +1,25 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
-class SavedMovie extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            
-        }
-    }
-
-    render() { 
-        return ( 
-            <tr>
-                <td>{this.props.number}</td>
-                <td>
-                    <img src={this.props.movie.poster} alt="" className="table-poster" />
-                </td>
-                <td>{this.props.movie.title}</td>
-                <td>{this.props.movie.genre}</td>
-                <td>{this.props.movie.type}</td>
-                <td>{this.props.movie.status}</td>
-                <td>
-                    <Link to={"/edit-movie-inforamtion/"+this.props.movie._id} className="table-edit-btn">Edit</Link> 
-                    <span> | </span> 
-                    <span className="table-remove-btn" onClick={() => { this.props.deleteMovie(this.props.movie._id) }}>Remove</span>
-                </td>
-            </tr>
-        );
-    }
+function SavedMovie(props) {
+    return ( 
+        <tr>
+            <td>{props.number}</td>
+            <td>
+                <img src={props.movie.poster} alt="" className="table-poster" />
+            </td>
+            <td>{props.movie.title}</td>
+            <td>{props.movie.genre}</td>
+            <td>{props.movie.type}</td>
+            <td>{props.movie.status}</td>
+            <td>
+                <Link to={"/edit-movie-inforamtion/"+props.movie._id} className="table-edit-btn">Edit</Link> 
+                <span> | </span> 
+                <span className="table-remove-btn" onClick={() => { props.deleteMovie(props.movie._id) }}>Remove</span>
+            </td>
+        </tr>
+    );
 }
 
 export default SavedMovie;
