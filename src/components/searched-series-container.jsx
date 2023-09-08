cc02ec6da2080bb739263e3f6411384dfe9706b5
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-09-08 20:06:13 +0200

    Searched-series is now a functional component

--- src/components/searched-series-container.jsx
+++ src/components/searched-series-container.jsx
@@ -1,81 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import '../App.css';
 
 import Movie from './show-card'
 
-class searchSeries extends Component {
-    constructor(props) {
-        super(props);
+function SearchSeries() {
+    const searchStringRef = useRef("");
+    const [searchedSeries, setSearchedSeries] = useState(null);
 
-        this.state = { 
-            series: [],
-            search: '',
-        }
-    }
-
-    seriesList() {
-        return this.state.series.map(currentObj => {
-            return <Movie movieObj={currentObj} key={currentObj.imdbID} />;
+    const seriesList = () => {
+        return searchedSeries.map(currentObj => {
+            return <Movie movieObj={currentObj} key={currentObj.imdbID} />
         })
     }
 
-    inputSearch = event => {
-        this.setState({ 
-            search: event.target.value.substr(0, 20) 
-        });
-    }
+    const handleOnSubmit = event => {
+        event.preventDefault()
 
-    onSubmit = event => {
-        event.preventDefault();
-
-        // Send an http post-request to the following endpoint & bring back info
-        axios.post('http://www.omdbapi.com/?apikey=71470024&s='+this.state.search+'&type=series')   
+        // send an http post-request to the following endpoint & bring back info
+        axios.post(`http://www.omdbapi.com/?apikey=71470024&s=${searchStringRef.current.value}&type=series`)
             .then(res => {
                 console.log(res.data)
-                // 
-                if(res.data.Search.length) {
-                    this.setState({ 
-                        series: res.data.Search
-                    })
-                } else {
-                    this.setState({ 
-                        defaultMess: "No Results Found"
-                    })
-                }
+                if(res.data.Response === "True") {
+                    setSearchedSeries(res.data.Search)
+                } 
+                else throw new Error('Search failed')
             })
-            .catch(function(error){
-                console.log(error);
-            });
-
-        this.setState({ 
-            // clean input-text-field for a new search
-            search: '',
-            // clear list for new results 
-            series: []
-        });
+            .catch(function(error) {
+                console.log(error)
+            })
     }
 
-    render() { 
-        return ( 
-            <div className="row">
-                <div className="search-shows-container">
-                    <form onSubmit={this.onSubmit}>
-                        <input type="text"
-                            required
-                            placeholder="Enter a series name"
-                            value={this.state.search}
-                            onChange={this.inputSearch}
-                        />
-                        <input type="submit" value="Search" className="stylish-btn"/>
-                    </form>
-                </div>
-                <div className="shows-container">
-                    {!this.state.series.length ? <h2 className="container-empty-heading">Search For Series</h2> : this.seriesList()}
-                </div>
-            </div> 
-        );
-    }
+    return ( 
+        <div className="row">
+            <div className="search-shows-container">
+                <form onSubmit={handleOnSubmit}>
+                    <input type="text" placeholder="Enter a series name" ref={searchStringRef} required />
+                    <input type="submit" value="Search" className="stylish-btn"/>
+                </form>
+            </div>
+            <div className="shows-container">
+                {!searchedSeries ? <h2 className="container-empty-heading">Search For Series</h2> : seriesList()}
+            </div>
+        </div>
+    );
 }
 
-export default searchSeries;
+export default SearchSeries;
