0610574e9a612d3e9f2fc2ae41d73981d80f6af3
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-09-08 16:45:47 +0200

    Removed empty lines

--- src/components/show-card.jsx
+++ src/components/show-card.jsx
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import { Link } from "react-router-dom";
 import '../App.css';
 
 function Movie(props) {
-
     return ( 
         <Link to={"/movies/"+props.movieObj.imdbID} className="container-movie-link">
             <img src={props.movieObj.Poster} alt="" />
